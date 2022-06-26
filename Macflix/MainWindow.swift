ce8a1482fb79096c012c94d9e8b9677225f95a6b
Waitsnake <942085+Waitsnake@users.noreply.github.com>
2022-06-26 23:39:03 +0200

    Enable title bar in full screen
    enable title bar in full screen to avoid this white bar that pops in when moving mouse to the top of screen

--- Macflix/MainWindow.swift
+++ Macflix/MainWindow.swift
@@ -1,6 +1,8 @@
 import Cocoa
 
 class MainWindow: NSWindow {
+    var fullScr = false
+    
     // Catches the build-in menuitems.
     // Use AppDelegate.validateMenuItem to catch custom menuitems.
 //    override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
@@ -15,5 +17,26 @@ class MainWindow: NSWindow {
     override func toggleFullScreen(_ sender: Any?) {
         (windowController as? WindowController)?.avoidance = .off
         super.toggleFullScreen(sender)
+        
+        if fullScr == true
+        {
+            // disable title bar in window mode
+            fullScr = false
+            titleVisibility = .hidden
+            titlebarAppearsTransparent = true
+            standardWindowButton(.closeButton)?.isHidden = true
+            standardWindowButton(.miniaturizeButton)?.isHidden = true
+            standardWindowButton(.zoomButton)?.isHidden = true
+        }
+        else
+        {
+            // enable title bar in full screen to avoid this white bar that pops in when moving mouse to the top of screen
+            fullScr = true
+            titleVisibility = .visible
+            titlebarAppearsTransparent = false
+            standardWindowButton(.closeButton)?.isHidden = false
+            standardWindowButton(.miniaturizeButton)?.isHidden = false
+            standardWindowButton(.zoomButton)?.isHidden = false
+        }
     }
 }
